efa3f594b6ee3fd8c731574948fa8305e9023326
Lingerew711 <se.lingerew.getie@gmail.com>
2021-04-20 21:54:13 +0300

    [login] layout added

--- application.py
+++ application.py
@@ -1,26 +1,21 @@
-import os
-
-from flask import Flask, session
-from flask_session import Session
+from flask import Flask, render_template, request, session, logging, url_for,redirect,flash, jsonify
 from sqlalchemy import create_engine
+from flask_session import Session
 from sqlalchemy.orm import scoped_session, sessionmaker
+from passlib.hash import sha256_crypt
 
+import hashlib
+import os
+import requests
+
+engine = create_engine("postgresql://postgres:linge531@localhost:5432/Book_store")
+
+db=scoped_session(sessionmaker(bind=engine))
 app = Flask(__name__)
 
 # Check for environment variable
-if not os.getenv("DATABASE_URL"):
-    raise RuntimeError("DATABASE_URL is not set")
-
-# Configure session to use filesystem
-app.config["SESSION_PERMANENT"] = False
-app.config["SESSION_TYPE"] = "filesystem"
-Session(app)
-
-# Set up database
-engine = create_engine(os.getenv("DATABASE_URL"))
-db = scoped_session(sessionmaker(bind=engine))
 
 
 @app.route("/")
 def index():
-    return "Project One: TODO"
+    return render_template('layout.html')
