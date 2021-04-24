ca11431f34f50555a9075a591810643bf15e10e4
Lingerew711 <se.lingerew.getie@gmail.com>
2021-04-24 10:19:56 +0300

    server chaged

--- application.py
+++ application.py
@@ -8,7 +8,7 @@ import hashlib
 import os
 import requests
 
-engine = create_engine("postgresql://postgres:linge531@localhost:5432/Book_store")
+engine = create_engine("postgresql://fhrgggnmrmrxkn:d9843ee1039f5c4b915f9e863e6bd74fabff6377b2933c78af4fde3845a2c7c6@ec2-52-21-252-142.compute-1.amazonaws.com:5432/dng8iia4pdh78")
 
 db=scoped_session(sessionmaker(bind=engine))
 
