06d255ab99ba9adb169bb1a93f920014f3d4b269
Lingerew711 <se.lingerew.getie@gmail.com>
2021-04-24 10:20:04 +0300

    server chaged

--- import.py
+++ import.py
@@ -1,7 +1,11 @@
 import csv
 import psycopg2
-conn = psycopg2.connect("postgresql://postgres:linge531@localhost:5432/Book_store")
+conn = psycopg2.connect("postgresql://fhrgggnmrmrxkn:d9843ee1039f5c4b915f9e863e6bd74fabff6377b2933c78af4fde3845a2c7c6@ec2-52-21-252-142.compute-1.amazonaws.com:5432/dng8iia4pdh78")
 cur = conn.cursor()
+sql_file = open("database.sql")
+sql_as_string = sql_file.read()
+cur.execute(sql_as_string)
+print("Congratulations")
 with open('books.csv', 'r') as f:
     reader = csv.reader(f)
     next(reader) # Skip the header row.
@@ -11,3 +15,4 @@ with open('books.csv', 'r') as f:
         row
     )
 conn.commit()
+print("Congratulations book inserted!")
